225722f3d40d3d7c3380fddc9076a047093d18e0
koae0522 <yantukeuma@gmail.com>
2023-08-20 22:23:00 +0900

    画面遷移実装

---
+++ app/src/main/java/com/example/annect/ui/ConnectScreen.kt
@@ -0,0 +1,18 @@
+package com.example.annect.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ConnectScreen(onHomeButtonClicked: ()->Unit = {}){
+    Column(){
+        Text("this is ConnectScreen")
+        Button(onClick = {onHomeButtonClicked()}) {
+            Text(text = "return to HomeScreen")
+        }
+    }
+
+
+}
--- app/src/main/java/com/example/annect/ui/theme/Shape.kt
+++ app/src/main/java/com/example/annect/ui/theme/Shape.kt
@@ -6,5 +6,5 @@ import androidx.compose.material3.Shapes
 import androidx.compose.ui.unit.dp
 
 val Shapes = Shapes(
-    medium = RoundedCornerShape(10.dp)
+    medium = RoundedCornerShape(topStart = 10.dp, bottomEnd = 10.dp)
 )
