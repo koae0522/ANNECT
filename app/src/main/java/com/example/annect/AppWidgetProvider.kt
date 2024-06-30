a818f373726bb521b4b6abb1d753fafe5e816c2e
koae0522 <yantukeuma@gmail.com>
2024-06-30 15:06:07 +0900

    widgetにanimaの画像を表示できるようにした

---
+++ app/src/main/java/com/example/annect/AppWidgetProvider.kt
@@ -0,0 +1,40 @@
+package com.example.annect
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.ComponentName
+import android.content.Context
+import android.util.Log
+import android.widget.RemoteViews
+import com.example.annect.data.AnimaData
+import com.example.annect.data.AnimaDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class MyAppWidgetProvider : AppWidgetProvider() {
+
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ){
+    Log.d("とんかつ", "onEnabled")
+        CoroutineScope(Dispatchers.Main).launch {
+            val data = withContext(Dispatchers.IO) {
+                val db = AnimaDatabase.buildAnimaDatabase(context)
+                val dao = db.animaDataDao()
+                dao.getFirstAnimaData()
+            }
+            Log.d("とんかつ",data.toString())
+            val appWidgetManager = AppWidgetManager.getInstance(context)
+            val views = RemoteViews(context.packageName, R.layout.widget_layout)
+            views.setImageViewResource(R.id.imageView1, data.body)
+            views.setImageViewResource(R.id.imageView2, data.eye)
+            views.setImageViewResource(R.id.imageView3, data.mouth)
+            views.setImageViewResource(R.id.imageView4, data.accessory)
+            appWidgetManager.updateAppWidget(appWidgetIds, views)
+        }
+    }
+}
